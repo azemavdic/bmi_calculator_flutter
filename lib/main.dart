c64cd7c8f844dbf83f64edc69ec165fe3b969dd6
unknown <asko2212@hotmail.com>
2021-04-22 08:58:26 +0200

    boxes added

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(),
       home: InputPage(),
     );
   }
@@ -23,11 +24,60 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                ReusableCard(colour: Colors.grey.shade800),
+                ReusableCard(colour: Colors.grey.shade800),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                ReusableCard(colour: Colors.grey.shade800),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                ReusableCard(colour: Colors.grey.shade800),
+                ReusableCard(colour: Colors.grey.shade800),
+              ],
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: colour,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 0.1),
+            ),
+          ],
+        ),
+        height: 200,
+        width: 170,
+        margin: EdgeInsets.all(10.0),
       ),
     );
   }
